a98db22779a65cf811ff2a1cf66a8c455d376fab
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-03-06 11:09:08 +0100

    fixed open-close

--- src/scripts/render/open-close-modalCoct.js
+++ src/scripts/render/open-close-modalCoct.js
@@ -6,18 +6,17 @@ const modalCoctailsEl = document.querySelector('.modal-coctails');
 const closeModal = document.querySelector('.close-modal');
 const apiId = new ApiService();
 
-async function handleOpenCloseModal(e) {
+export async function handleOpenCloseModal(e) {
   const idCard = e.target.clouser('.card').id;
   const data = await apiId.fetchDataById(idCard);
   renderModalCoctails(data);
-
-  backdrop.classList.toggle('is-hidden');
-  modalCoctailsEl.classList.toggle('is-hidden');
-  const ingridient = document
+  backdrop.classList.remove('is-hidden');
+  modalCoctailsEl.classList.remove('is-hidden');
+  document
     .querySelectorAll('.ingridients-item')
     .forEach(el => el.addEventListener('click', handleOpenModalIngridients));
 }
 closeModal.addEventListener('click', () => {
-  backdrop.classList.toggle('is-hidden');
-  modalCoctailsEl.classList.toggle('is-hidden');
+  backdrop.classList.add('is-hidden');
+  modalCoctailsEl.classList.add('is-hidden');
 });
--- src/scripts/render/render-modal-coctail.js
+++ src/scripts/render/render-modal-coctail.js
@@ -17,9 +17,9 @@ export function renderModalCoctails(data) {
     <h3 class="ingridients">INGREDIENTS</h3>
     <h3 class="ingridients-per">Per cocktail</h3>
     <ul class="ingridients-list">
-     ${
-       data.strIngredient1
-         ? `<li class="ingridients-item">
+      ${
+        data.strIngredient1
+          ? `<li class="ingridients-item">
         <a
           href="#"
           class="ingridients-link"
@@ -27,12 +27,176 @@ export function renderModalCoctails(data) {
           rel="noreferrer noopener"
           >✶ ${data.strMeasure1} ${data.strIngredient1}</a
         >`
+          : ''
+      }
+     ${
+       data.strIngredient2
+         ? `<li class="ingridients-item">
+        <a
+          href="#"
+          class="ingridients-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          >✶ ${data.strMeasure2} ${data.strIngredient2}</a
+        >`
          : ''
      }
-    
-
-        
-    </ul>
+${
+  data.strIngredient3
+    ? `<li class="ingridients-item">
+        <a
+          href="#"
+          class="ingridients-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          >✶ ${data.strMeasure3} ${data.strIngredient3}</a
+        >`
+    : ''
+}
+${
+  data.strIngredient4
+    ? `<li class="ingridients-item">
+        <a
+          href="#"
+          class="ingridients-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          >✶ ${data.strMeasure4} ${data.strIngredient4}</a
+        >`
+    : ''
+}
+${
+  data.strIngredient5
+    ? `<li class="ingridients-item">
+        <a
+          href="#"
+          class="ingridients-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          >✶ ${data.strMeasure5} ${data.strIngredient5}</a
+        >`
+    : ''
+}
+${
+  data.strIngredient6
+    ? `<li class="ingridients-item">
+        <a
+          href="#"
+          class="ingridients-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          >✶ ${data.strMeasure6} ${data.strIngredient6}</a
+        >`
+    : ''
+}
+${
+  data.strIngredient7
+    ? `<li class="ingridients-item">
+        <a
+          href="#"
+          class="ingridients-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          >✶ ${data.strMeasure7} ${data.strIngredient7}</a
+        >`
+    : ''
+}
+ ${
+   data.strIngredient8
+     ? `<li class="ingridients-item">
+        <a
+          href="#"
+          class="ingridients-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          >✶ ${data.strMeasure8} ${data.strIngredient8}</a
+        >`
+     : ''
+ }
+ ${
+   data.strIngredient9
+     ? `<li class="ingridients-item">
+<a
+          href="#"
+          class="ingridients-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          >✶ ${data.strMeasure9} ${data.strIngredient9}</a
+        >`
+     : ''
+ }
+${
+  data.strIngredient10
+    ? `<li class="ingridients-item">
+        <a
+          href="#"
+          class="ingridients-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          >✶ ${data.strMeasure10} ${data.strIngredient10}</a
+        >`
+    : ''
+}
+${
+  data.strIngredient11
+    ? `<li class="ingridients-item"><a
+          href="#"
+          class="ingridients-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          >✶ ${data.strMeasure11} ${data.strIngredient11}</a
+        >`
+    : ''
+}
+ ${
+   data.strIngredient12
+     ? `<li class="ingridients-item">
+        <a
+          href="#"
+          class="ingridients-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          >✶ ${data.strMeasure12} ${data.strIngredient12}</a
+        >`
+     : ''
+ }
+ ${
+   data.strIngredient13
+     ? `<li class="ingridients-item">
+        <a
+          href="#"
+          class="ingridients-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          >✶ ${data.strMeasure13} ${data.strIngredient13}</a
+        >`
+     : ''
+ }
+ ${
+   data.strIngredient14
+     ? `<li class="ingridients-item">
+        <a
+          href="#"
+          class="ingridients-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          >✶ ${data.strMeasure14} ${data.strIngredient14}</a
+        >`
+     : ''
+ }
+  ${
+    data.strIngredient15
+      ? `<li class="ingridients-item">
+        <a
+          href="#"
+          class="ingridients-link"
+          target="_blank"
+          rel="noreferrer noopener"
+          >✶ ${data.strMeasure15} ${data.strIngredient15}</a
+        >`
+      : ''
+  }
+</ul>
       <button type="button" class="button-add">Add to favorite</button>
       `;
     })
