da9eb3b2acb5ba1b86cf20edf1ea6e285677154a
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-03-06 11:14:13 +0100

    1

--- src/scripts/render/open-close-modalCoct.js
+++ src/scripts/render/open-close-modalCoct.js
@@ -9,13 +9,17 @@ const apiId = new ApiService();
 export async function handleOpenCloseModal(e) {
   const idCard = e.target.clouser('.card').id;
   const data = await apiId.fetchDataById(idCard);
+
   renderModalCoctails(data);
+
   backdrop.classList.remove('is-hidden');
   modalCoctailsEl.classList.remove('is-hidden');
+
   document
     .querySelectorAll('.ingridients-item')
     .forEach(el => el.addEventListener('click', handleOpenModalIngridients));
 }
+
 closeModal.addEventListener('click', () => {
   backdrop.classList.add('is-hidden');
   modalCoctailsEl.classList.add('is-hidden');
