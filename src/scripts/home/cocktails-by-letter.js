a8610062edd55c63d09a09bfb85c7948848f5b91
Oleksii Savchenko <savchenko.orient@gmail.com>
2023-03-07 17:53:28 +0200

    fix pagination and  add event listeners to paginatio and gallery

--- src/scripts/home/cocktails-by-letter.js
+++ src/scripts/home/cocktails-by-letter.js
@@ -1,6 +1,4 @@
-import ApiService from '../api/apiService';
-import Render from '../render/render';
-import Gallery from '../gallery/build-gallery';
+import Gallery from '../gallery/gallery';
 
 const el = {
   letterBox: document.querySelector('.search-container'),
@@ -9,38 +7,16 @@ const el = {
 
 el.letterBox.addEventListener('click', handleClickToLetter);
 
-const screenWidth = window.innerWidth;
-const getData = new ApiService();
-const render = new Render();
 const gallery = new Gallery();
-
 async function handleClickToLetter(e) {
   if (e.target.classList.value !== 'search-box') {
     return;
   }
 
-  removeCocktails();
+  gallery.clearGallery();
 
   const selectedLetter = e.target.textContent;
-  const numberOfCocktails = gallery.numberOfItemsPerPage(screenWidth);
-  const cocktailsDataBySelectedLetter = await getData.fetchDataByLetter(
-    selectedLetter
-  );
-  console.log(selectedLetter);
-  gallery.numberOfItemsPerPage(screenWidth);
-  gallery.getDataByLetter(selectedLetter);
-
-  //   for (let i = 0; i < numberOfCocktails; i++) {
-  //     gallery.promises.push(cocktailsDataBySelectedLetter);
-  //   }
-
-  //   console.log(cocktailsDataBySelectedLetter);
-  //   console.log(numberOfCocktails);
-  //   console.log(gallery.promises);
-
-  //   gallery.promises = [];
-}
-
-function removeCocktails() {
-  el.gallery.innerHTML = '';
+  gallery.numberOfItemsPerPage();
+  const data = await gallery.getDataByLetter(selectedLetter);
+  gallery.setCurrentPage(1, data);
 }
