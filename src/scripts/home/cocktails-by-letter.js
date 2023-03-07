2680a42feba3b4270adcb2e07be14a098e9a10d2
Oleksii Savchenko <savchenko.orient@gmail.com>
2023-03-07 20:47:13 +0200

    add notFound block

--- src/scripts/home/cocktails-by-letter.js
+++ src/scripts/home/cocktails-by-letter.js
@@ -13,35 +13,35 @@ el.letterBox.addEventListener('click', handleClickToLetter);
 
 const gallery = new Gallery();
 async function handleClickToLetter(e) {
-const screenWidth = window.innerWidth;
-// const getData = new ApiService();
-// const render = new Render();
-const gallery = new Gallery();
+  const screenWidth = window.innerWidth;
+  // const getData = new ApiService();
+  // const render = new Render();
+  const gallery = new Gallery();
 
-el.letterBox.addEventListener('click', handleClickToLetterRender);
-el.openListLetter.addEventListener('click', OpenCloseListOfLetter);
-el.selectLetterMb.addEventListener('click', handleClickToLetterMobileRender);
+  el.letterBox.addEventListener('click', handleClickToLetterRender);
+  el.openListLetter.addEventListener('click', OpenCloseListOfLetter);
+  el.selectLetterMb.addEventListener('click', handleClickToLetterMobileRender);
 
-async function handleClickToLetterRender(e) {
-  if (e.target.classList.value !== 'search-box') {
-    return;
+  async function handleClickToLetterRender(e) {
+    if (e.target.classList.value !== 'search-box') {
+      return;
+    }
+
+    gallery.clearGallery();
+    const activeLetter = document.querySelector('.search-box.is-active');
+
+    if (activeLetter) {
+      activeLetter.classList.remove('is-active');
+    }
+
+    removeCocktails();
+
+    const selectedElement = e.target;
+    const selectedLetter = e.target.textContent;
+    gallery.numberOfItemsPerPage();
+    const data = await gallery.getDataByLetter(selectedLetter);
+    gallery.setCurrentPage(1, data);
   }
-
-  gallery.clearGallery();
-  const activeLetter = document.querySelector('.search-box.is-active');
-
-  if (activeLetter) {
-    activeLetter.classList.remove('is-active');
-  }
-
-  removeCocktails();
-
-  const selectedElement = e.target;
-  const selectedLetter = e.target.textContent;
-  gallery.numberOfItemsPerPage();
-  const data = await gallery.getDataByLetter(selectedLetter);
-  gallery.setCurrentPage(1, data);
-}
 
   selectedElement.classList.add('is-active');
   console.log(selectedElement);
