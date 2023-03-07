7f525f800faf9462cc9c457d8989833b7e6cf613
BogdanYeshchenko <b.yeshchenko@gmail.com>
2023-03-07 20:17:22 +0200

    !!!!!!!!!!

--- src/scripts/home/cocktails-by-letter.js
+++ src/scripts/home/cocktails-by-letter.js
@@ -13,75 +13,102 @@ el.letterBox.addEventListener('click', handleClickToLetter);
 
 const gallery = new Gallery();
 async function handleClickToLetter(e) {
-const screenWidth = window.innerWidth;
-// const getData = new ApiService();
-// const render = new Render();
-const gallery = new Gallery();
-
-el.letterBox.addEventListener('click', handleClickToLetterRender);
-el.openListLetter.addEventListener('click', OpenCloseListOfLetter);
-el.selectLetterMb.addEventListener('click', handleClickToLetterMobileRender);
-
-async function handleClickToLetterRender(e) {
   if (e.target.classList.value !== 'search-box') {
     return;
   }
 
   gallery.clearGallery();
-  const activeLetter = document.querySelector('.search-box.is-active');
 
-  if (activeLetter) {
-    activeLetter.classList.remove('is-active');
-  }
-
-  removeCocktails();
-
-  const selectedElement = e.target;
   const selectedLetter = e.target.textContent;
   gallery.numberOfItemsPerPage();
   const data = await gallery.getDataByLetter(selectedLetter);
   gallery.setCurrentPage(1, data);
 }
 
-  selectedElement.classList.add('is-active');
-  console.log(selectedElement);
+// import Gallery from '../gallery/gallery';
 
-  gallery.numberOfItemsPerPage(screenWidth);
-  gallery.getDataByLetter(selectedLetter);
-}
+// const el = {
+//   letterBox: document.querySelector('.search-container'),
+//   gallery: document.querySelector('.gallery__wrapper'),
+//   openListLetter: document.querySelector('.open-list-letter'),
+//   selectLetterMb: document.querySelector('.select-letter-mb'),
+//   selectidLetterMbBox: document.querySelector('.selectid-letter-mb-box'),
+//   selectidLetterMb: document.querySelector('.selectid-letter-mb'),
+// };
 
-async function handleClickToLetterMobileRender(e) {
-  if (e.target.classList.value !== 'search-box-mb') {
-    return;
-  }
+// el.letterBox.addEventListener('click', handleClickToLetter);
 
-  const selectedLetter = e.target.textContent;
+// const gallery = new Gallery();
+// async function handleClickToLetter(e) {
+//   const screenWidth = window.innerWidth;
+// const getData = new ApiService();
+// const render = new Render();
+// const gallery = new Gallery();
 
-  removeCocktails();
-  OpenCloseListOfLetter();
-  assingContentBySelected(selectedLetter);
+// el.letterBox.addEventListener('click', handleClickToLetterRender);
+// el.openListLetter.addEventListener('click', OpenCloseListOfLetter);
+// el.selectLetterMb.addEventListener('click', handleClickToLetterMobileRender);
 
-  gallery.numberOfItemsPerPage(screenWidth);
-  gallery.getDataByLetter(selectedLetter);
-}
+// async function handleClickToLetterRender(e) {
+//   if (e.target.classList.value !== 'search-box') {
+//     return;
+//   }
 
-function removeCocktails() {
-  el.gallery.innerHTML = '';
-}
+//   gallery.clearGallery();
+//   const activeLetter = document.querySelector('.search-box.is-active');
 
-function OpenCloseListOfLetter() {
-  if (el.selectidLetterMbBox.classList.contains('selectid')) {
-    el.selectidLetterMbBox.classList.remove('selectid');
-  }
+//   if (activeLetter) {
+//     activeLetter.classList.remove('is-active');
+//   }
 
-  el.selectLetterMb.classList.toggle('hiden');
-  el.openListLetter.classList.toggle('open');
-}
+//   removeCocktails();
 
-function assingContentBySelected(content) {
-  el.selectidLetterMb.textContent = content;
-  el.selectidLetterMbBox.classList.add('selectid');
-}
+//   const selectedElement = e.target;
+//   const selectedLetter = e.target.textContent;
+//   gallery.numberOfItemsPerPage();
+//   const data = await gallery.getDataByLetter(selectedLetter);
+//   gallery.setCurrentPage(1, data);
+
+//   selectedElement.classList.add('is-active');
+//   console.log(selectedElement);
+// }
+
+// gallery.numberOfItemsPerPage(screenWidth);
+// gallery.getDataByLetter(selectedLetter);
+// }
+
+// async function handleClickToLetterMobileRender(e) {
+//   if (e.target.classList.value !== 'search-box-mb') {
+//     return;
+//   }
+
+//   const selectedLetter = e.target.textContent;
+
+//   removeCocktails();
+//   OpenCloseListOfLetter();
+//   assingContentBySelected(selectedLetter);
+
+// gallery.numberOfItemsPerPage(screenWidth);
+// gallery.getDataByLetter(selectedLetter);
+// }
+
+// function removeCocktails() {
+//   el.gallery.innerHTML = '';
+// }
+
+// function OpenCloseListOfLetter() {
+//   if (el.selectidLetterMbBox.classList.contains('selectid')) {
+//     el.selectidLetterMbBox.classList.remove('selectid');
+//   }
+
+//   el.selectLetterMb.classList.toggle('hiden');
+//   el.openListLetter.classList.toggle('open');
+// }
+
+// function assingContentBySelected(content) {
+//   el.selectidLetterMb.textContent = content;
+//   el.selectidLetterMbBox.classList.add('selectid');
+// }
 
 // const selectidLetterMbBoxElement = document.querySelector('.selectid-letter-mb-box')
 // const selectidLetterMbElement =document.querySelector('.select-letter-mb');
