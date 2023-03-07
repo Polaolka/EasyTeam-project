a1f3c0f97fda490b32e8c7b2aaa9cb5373761e23
Paul <pawel.graur47@gmail.com>
2023-03-07 16:54:29 +0200

    Fix Footer and change orange footer

--- src/scripts/common/footer.js
+++ src/scripts/common/footer.js
@@ -1,13 +1,33 @@
-const modalBtn = document.querySelector('[modalBtn]');
-const backdrop = document.querySelector('.backdrop');
-const closeBtn = document.querySelector('#closeBtn');
+import { openModal } from './render_footer_modal';
 
-// Відкриваємо модальне вікно
-modalBtn.addEventListener('click', () => {
-  backdrop.classList.remove('is-hidden');
-});
+const refs = {
+  footerModal: document.querySelector('.footer-modal'),
+  openModalLink: document.querySelector('[data-footer-open]'),
+  modal: document.querySelector('[data-modal-footer]'),
+  backdrop: document.querySelector('.js-backdrop'),
+  addBodyClass: document.querySelector('body'),
+};
 
-// Закриваємо модальне вікно
-closeBtn.addEventListener('click', () => {
-  backdrop.classList.add('is-hidden');
-});
+refs.openModalLink.addEventListener('click', openModal);
+refs.backdrop.addEventListener('click', onBackdropClick);
+
+export function onClickEscape(event) {
+  if (event.key === 'Escape') {
+    console.log(event.key);
+    refs.modal.classList.add('is-hidden');
+    refs.addBodyClass.classList.remove('modal-open');
+    document.removeEventListener('keydown', onClickEscape);
+  }
+}
+
+export function closeModal() {
+  refs.modal.classList.add('is-hidden');
+  refs.addBodyClass.classList.remove('modal-open');
+  document.removeEventListener('keydown', onClickEscape);
+}
+
+function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    closeModal();
+  }
+}
--- src/scripts/favorite.js
+++ src/scripts/favorite.js
@@ -5,5 +5,8 @@ import './common/mobile-menu';
 import './common/theme-toggler';
 import './modals/open-close-modal';
 import './common/footer';
+import './common/input-validation';
+import './common/render_footer_modal';
+import './common/slider_glide';
 import './common/dropdown-menu-mob';
 import './common/theme-toggler-mobile';
