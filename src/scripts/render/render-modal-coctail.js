847454ebbfdbdaab692018107af15cb3da1569e6
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-03-08 09:43:41 +0100

    fixed

--- src/scripts/render/render-modal-coctail.js
+++ src/scripts/render/render-modal-coctail.js
@@ -40,7 +40,7 @@ function templateIngridients(data) {
           class="ingridients-link"
           target="_blank"
           rel="noreferrer noopener"
-          ><span id="coc-mark">✶</span> ${data[key2]} <span class="ingr-value">${data[key1]}</span></a
+          >✶ ${data[key2]} ${data[key1]}</a
         >`;
   }
 
